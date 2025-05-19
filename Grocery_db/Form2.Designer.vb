e0ca301a76605145aff88c20b3b8f4c0c2c09e59
ptrpu1 <patrick.ashley.pacoma@adamson.edu.ph>
2025-05-19 18:54:01 +0800

    Revert "Added form 4 (admin form) ui and basic functions"
    This reverts commit a570ca6b4ba3b38c5e54ba44799818677533dce8.

--- Grocery_db/Form2.Designer.vb
+++ Grocery_db/Form2.Designer.vb
@@ -43,10 +43,9 @@ Partial Class Form2
         ' tb_firstname
         ' 
         tb_firstname.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        tb_firstname.Location = New Point(37, 173)
-        tb_firstname.Margin = New Padding(3, 4, 3, 4)
+        tb_firstname.Location = New Point(32, 130)
         tb_firstname.Name = "tb_firstname"
-        tb_firstname.Size = New Size(167, 25)
+        tb_firstname.Size = New Size(147, 21)
         tb_firstname.TabIndex = 0
         ' 
         ' Label1
@@ -55,9 +54,9 @@ Partial Class Form2
         Label1.BackColor = Color.Transparent
         Label1.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = Color.DimGray
-        Label1.Location = New Point(224, 239)
+        Label1.Location = New Point(196, 179)
         Label1.Name = "Label1"
-        Label1.Size = New Size(77, 18)
+        Label1.Size = New Size(66, 15)
         Label1.TabIndex = 1
         Label1.Text = "Username"
         ' 
@@ -67,10 +66,9 @@ Partial Class Form2
         Btn_create.FlatStyle = FlatStyle.Popup
         Btn_create.Font = New Font("Georgia", 13.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Btn_create.ForeColor = SystemColors.ButtonHighlight
-        Btn_create.Location = New Point(112, 435)
-        Btn_create.Margin = New Padding(3, 4, 3, 4)
+        Btn_create.Location = New Point(98, 326)
         Btn_create.Name = "Btn_create"
-        Btn_create.Size = New Size(206, 59)
+        Btn_create.Size = New Size(180, 44)
         Btn_create.TabIndex = 2
         Btn_create.Text = "Create Account"
         Btn_create.UseVisualStyleBackColor = False
@@ -81,9 +79,9 @@ Partial Class Form2
         Label2.BackColor = Color.Transparent
         Label2.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label2.ForeColor = Color.DimGray
-        Label2.Location = New Point(37, 152)
+        Label2.Location = New Point(32, 114)
         Label2.Name = "Label2"
-        Label2.Size = New Size(83, 18)
+        Label2.Size = New Size(72, 15)
         Label2.TabIndex = 4
         Label2.Text = "First Name"
         ' 
@@ -93,19 +91,18 @@ Partial Class Form2
         Label3.BackColor = Color.Transparent
         Label3.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label3.ForeColor = Color.DimGray
-        Label3.Location = New Point(37, 335)
+        Label3.Location = New Point(32, 251)
         Label3.Name = "Label3"
-        Label3.Size = New Size(71, 18)
+        Label3.Size = New Size(60, 15)
         Label3.TabIndex = 8
         Label3.Text = "Password"
         ' 
         ' tb_password
         ' 
         tb_password.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        tb_password.Location = New Point(37, 360)
-        tb_password.Margin = New Padding(3, 4, 3, 4)
+        tb_password.Location = New Point(32, 270)
         tb_password.Name = "tb_password"
-        tb_password.Size = New Size(167, 25)
+        tb_password.Size = New Size(147, 21)
         tb_password.TabIndex = 7
         ' 
         ' Label4
@@ -114,19 +111,18 @@ Partial Class Form2
         Label4.BackColor = Color.Transparent
         Label4.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label4.ForeColor = Color.DimGray
-        Label4.Location = New Point(224, 152)
+        Label4.Location = New Point(196, 114)
         Label4.Name = "Label4"
-        Label4.Size = New Size(81, 18)
+        Label4.Size = New Size(69, 15)
         Label4.TabIndex = 6
         Label4.Text = "Last Name"
         ' 
         ' tb_username
         ' 
         tb_username.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        tb_username.Location = New Point(224, 261)
-        tb_username.Margin = New Padding(3, 4, 3, 4)
+        tb_username.Location = New Point(196, 196)
         tb_username.Name = "tb_username"
-        tb_username.Size = New Size(167, 25)
+        tb_username.Size = New Size(147, 21)
         tb_username.TabIndex = 5
         ' 
         ' Label6
@@ -135,9 +131,9 @@ Partial Class Form2
         Label6.BackColor = Color.Transparent
         Label6.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label6.ForeColor = Color.DimGray
-        Label6.Location = New Point(37, 239)
+        Label6.Location = New Point(32, 179)
         Label6.Name = "Label6"
-        Label6.Size = New Size(48, 18)
+        Label6.Size = New Size(42, 15)
         Label6.TabIndex = 10
         Label6.Text = "Email"
         ' 
@@ -145,10 +141,9 @@ Partial Class Form2
         ' 
         dtp_birthday.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         dtp_birthday.Format = DateTimePickerFormat.Short
-        dtp_birthday.Location = New Point(224, 357)
-        dtp_birthday.Margin = New Padding(3, 4, 3, 4)
+        dtp_birthday.Location = New Point(196, 268)
         dtp_birthday.Name = "dtp_birthday"
-        dtp_birthday.Size = New Size(228, 25)
+        dtp_birthday.Size = New Size(200, 21)
         dtp_birthday.TabIndex = 15
         ' 
         ' Label7
@@ -157,9 +152,9 @@ Partial Class Form2
         Label7.BackColor = Color.Transparent
         Label7.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label7.ForeColor = Color.DimGray
-        Label7.Location = New Point(224, 335)
+        Label7.Location = New Point(196, 251)
         Label7.Name = "Label7"
-        Label7.Size = New Size(94, 18)
+        Label7.Size = New Size(81, 15)
         Label7.TabIndex = 16
         Label7.Text = "Date of Birth"
         ' 
@@ -169,10 +164,9 @@ Partial Class Form2
         RbAdmin.BackColor = Color.Transparent
         RbAdmin.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         RbAdmin.ForeColor = Color.DimGray
-        RbAdmin.Location = New Point(37, 105)
-        RbAdmin.Margin = New Padding(3, 4, 3, 4)
+        RbAdmin.Location = New Point(32, 79)
         RbAdmin.Name = "RbAdmin"
-        RbAdmin.Size = New Size(74, 22)
+        RbAdmin.Size = New Size(65, 19)
         RbAdmin.TabIndex = 17
         RbAdmin.TabStop = True
         RbAdmin.Text = "Admin"
@@ -181,19 +175,17 @@ Partial Class Form2
         ' tb_lastname
         ' 
         tb_lastname.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        tb_lastname.Location = New Point(224, 173)
-        tb_lastname.Margin = New Padding(3, 4, 3, 4)
+        tb_lastname.Location = New Point(196, 130)
         tb_lastname.Name = "tb_lastname"
-        tb_lastname.Size = New Size(167, 25)
+        tb_lastname.Size = New Size(147, 21)
         tb_lastname.TabIndex = 19
         ' 
         ' tb_email
         ' 
         tb_email.Font = New Font("Georgia", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        tb_email.Location = New Point(37, 261)
-        tb_email.Margin = New Padding(3, 4, 3, 4)
+        tb_email.Location = New Point(32, 196)
         tb_email.Name = "tb_email"
-        tb_email.Size = New Size(167, 25)
+        tb_email.Size = New Size(147, 21)
         tb_email.TabIndex = 20
         ' 
         ' Label5
@@ -202,19 +194,18 @@ Partial Class Form2
         Label5.BackColor = Color.Transparent
         Label5.Font = New Font("Georgia", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label5.ForeColor = Color.DimGray
-        Label5.Location = New Point(61, 36)
+        Label5.Location = New Point(53, 27)
         Label5.Name = "Label5"
-        Label5.Size = New Size(345, 46)
+        Label5.Size = New Size(274, 38)
         Label5.TabIndex = 21
         Label5.Text = "Registration Form"
         ' 
         ' RbUser
         ' 
         RbUser.AutoSize = True
-        RbUser.Location = New Point(207, 105)
-        RbUser.Margin = New Padding(3, 4, 3, 4)
+        RbUser.Location = New Point(181, 79)
         RbUser.Name = "RbUser"
-        RbUser.Size = New Size(59, 24)
+        RbUser.Size = New Size(48, 19)
         RbUser.TabIndex = 22
         RbUser.TabStop = True
         RbUser.Text = "User"
@@ -222,10 +213,10 @@ Partial Class Form2
         ' 
         ' Form2
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImageLayout = ImageLayout.Stretch
-        ClientSize = New Size(914, 600)
+        ClientSize = New Size(800, 450)
         Controls.Add(RbUser)
         Controls.Add(Label5)
         Controls.Add(tb_email)
@@ -242,7 +233,6 @@ Partial Class Form2
         Controls.Add(Btn_create)
         Controls.Add(Label1)
         Controls.Add(tb_firstname)
-        Margin = New Padding(3, 4, 3, 4)
         Name = "Form2"
         Text = "Registration Form"
         ResumeLayout(False)
